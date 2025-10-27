0027435121b6db7ced2d7ae44010503091874ba1
Sougoumay <elidjaihamid@gmail.com>
2025-10-27 22:48:38 +0100

    harmonisation des noms de variables du bash setup.sh et les variables envoyés par le terraform

--- deployments/setup.sh
+++ deployments/setup.sh
@@ -3,11 +3,11 @@
 set -e
 
 # Variables injectées depuis Terraform
-DB_HOST="${db_endpoint}"
-DB_USER="${db_user}"
-DB_NAME="${db_name}"
-DB_PASSWORD="${db_password}"
-ALB_DOMAIN="${alb_domain}"
+db_endpoint="${db_endpoint}"
+db_user="${db_user}"
+db_name="${db_name}"
+db_password="${db_password}"
+alb_domain="${alb_domain}"
 
 echo "===== Installation des dépendances ====="
 apt update && apt upgrade -y
@@ -15,7 +15,7 @@ apt install -y git openjdk-17-jdk nginx
 
 echo "===== Clonage du repo ====="
 cd /opt
-git clone https://github.com/Sougoumay/social_media
+git clone https://github.com/Sougoumay/social_media/tree/deploy-ec2-alb-rds
 cd social_media
 chmod +x mvnw
 ./mvnw -DskipTests package
@@ -24,9 +24,10 @@ chmod +x mvnw
 echo "===== Configuration Spring App ====="
 mkdir -p /etc/springapp
 cat <<EOF > /etc/springapp/springapp.env
-DB_HOST=${DB_HOST}
-DB_USER=${DB_USER}
-DB_PASSWORD=${DB_PASSWORD}
+DB_USER=${db_user}
+DB_PASSWORD=${db_password}
+DB_HOST=${db_endpoint}
+DB_NAME=${db_name}
 DB_PORT=3306
 SPRING_PROFILES_ACTIVE=prod
 SERVER_PORT=8080
@@ -61,7 +62,7 @@ echo "===== Configuration Nginx ====="
 cat <<EOF > /etc/nginx/sites-available/socialmedia
 server {
     listen 80;
-    server_name ${ALB_DOMAIN};
+    server_name ${alb_domain};
 
     location / {
         proxy_pass http://127.0.0.1:8080;
